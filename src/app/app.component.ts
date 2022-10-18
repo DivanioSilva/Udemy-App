050b3bbe669fc6ab960803914f197a9aeece2fe2
Divanio Silva <dcsilva@mail.ru>
2022-10-18 18:11:45 +0100

    Add routing between welcome and products pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: "<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>"
+  template: `
+    <nav class="navbar navbar-expand" navbar-light bg-light>
+      <a class="navbar-brand">{{pageTitle}}</a>
+      <ul class="nav nav-pills">
+        <li><a class="nav-link" routerLink="/welcome">Home</a></li>
+        <li><a class="nav-link" routerLink="/products">Product List</a></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `
+    //"<div><h1>{{pageTitle}}</h1></div>"
 })
 export class AppComponent {
   pageTitle = 'Acme Product Managment';
