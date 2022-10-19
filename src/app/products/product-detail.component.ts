b253a6607ba5d924599503c340ee1fb9d7969f54
Divanio Silva <dcsilva@mail.ru>
2022-10-19 20:13:33 +0100

    Add a hardcoded product on product details

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {IProduct} from "./product";
 
 @Component({
   selector: 'app-product-detail',
@@ -8,12 +9,23 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle = "Product Detail";
+  product: IProduct | undefined;
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
+    this.product = {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "price": 8.9,
+      "description": "Curved claw steel hammer",
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.jpeg"
+    };
   }
 
   onBack(): void{
